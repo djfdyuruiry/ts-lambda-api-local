e5ab8e6c3ad71b8d108ea8c47bbb75ceee0c1e3a
matthew snoddy <djfdyuruiry@users.noreply.github.com>
2019-04-22 17:14:57 +0100

    v0.4.2 (#8)
    - Bumping `typescript-lambda-api` to v0.4.2
    - Adding logging
    - Adding typedoc
    - Updating readme with links for npm, github and latest typedoc
    - Pulling in updated test script and build commands

--- tests/src/test-components/TestAuthFilter.ts
+++ tests/src/test-components/TestAuthFilter.ts
@@ -2,6 +2,8 @@ import { BasicAuth, BasicAuthFilter } from "typescript-lambda-api"
 import { TestPrincipal } from './TestPrincipal';
 
 export class TestAuthFilter extends BasicAuthFilter<TestPrincipal> {
+    public readonly name = TestAuthFilter.name
+
     public async authenticate(basicAuth: BasicAuth): Promise<TestPrincipal> {
         if (basicAuth.username === "user" && basicAuth.password === "pass") {
             return new TestPrincipal()
