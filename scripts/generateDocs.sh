372ab93f25e032725ee5e929623e104561008ab6
matthew snoddy <djfdyuruiry@users.noreply.github.com>
2019-05-26 01:09:05 +0100

    v0.6.1
    - Upgrading `ts-lambda-api` to v0.6.1 
    - Fixing issues with docs including invalid paths for `ts-lambda-api`

---
+++ scripts/generateDocs.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+set -e
+
+projectGithubUrl="https://github.com/djfdyuruiry/ts-lambda-api-local/blob/master/src"
+tlaGithubUrl="https://github.com/djfdyuruiry/ts-lambda-api/blob/master/src"
+docHtmlPath="./docs/classes/apiconsoleapp.html"
+projectPath="$(pwd)"
+
+function cleanUpDocUrls() {
+    sed -i "s^${projectGithubUrl}${projectPath}/node_modules/ts-lambda-api/dist/^${tlaGithubUrl}/^" "${docHtmlPath}"
+    sed -i "s^${projectPath}/node_modules/ts-lambda-api/dist/^^" "${docHtmlPath}"
+    sed -E -i "s/.d.ts:[0-9]+/.ts/" "${docHtmlPath}"
+    sed -E -i "s/.d.ts#L[0-9]+/.ts/" "${docHtmlPath}"
+}
+
+function generateTypedoc() {
+    rm -rf ./docs
+
+    typedoc --mode file \
+        --excludePrivate \
+        --sourcefile-url-prefix "${projectGithubUrl}" \
+        --out ./docs
+}
+
+function generateDocs() {
+    generateTypedoc
+    cleanUpDocUrls
+}
+
+generateDocs
