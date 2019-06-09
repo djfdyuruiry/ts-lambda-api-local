eb59e695e5fdfc8a3a21c187262d3328751bf931
matthew snoddy <djfdyuruiry@users.noreply.github.com>
2019-06-09 22:36:25 +0100

    v0.6.2
    - Updating ts-lambda-api to v0.6.2
    - Upgrading other dependencies

--- scripts/generateDocs.sh
+++ scripts/generateDocs.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 set -e
 
-projectGithubUrl="https://github.com/djfdyuruiry/ts-lambda-api-local/blob/master/src/"
-tlaGithubUrl="https://github.com/djfdyuruiry/ts-lambda-api/blob/master/src/"
+projectGithubUrl="https://github.com/djfdyuruiry/ts-lambda-api-local/blob/master/src"
+tlaGithubUrl="https://github.com/djfdyuruiry/ts-lambda-api/blob/master/src"
 docHtmlPath="./docs/classes/apiconsoleapp.html"
 projectPath="$(pwd)"
 
 function cleanUpDocUrls() {
-    sed -i "s^${projectGithubUrl}${projectPath}/node_modules/ts-lambda-api/dist/^${tlaGithubUrl}^" "${docHtmlPath}"
+    sed -i "s^${projectGithubUrl}${projectPath}/node_modules/ts-lambda-api/dist/^${tlaGithubUrl}/^" "${docHtmlPath}"
     sed -i "s^${projectPath}/node_modules/ts-lambda-api/dist/^^" "${docHtmlPath}"
     sed -E -i "s/.d.ts:[0-9]+/.ts/" "${docHtmlPath}"
     sed -E -i "s/.d.ts#L[0-9]+/.ts/" "${docHtmlPath}"
