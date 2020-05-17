33c754afe94140f0c2f3956cedb1f234e90f1352
djfdyuruiry <djfdyuruiry@noreply.github.com>
2020-05-17 01:27:16 +0100

    Patching ts-lambda-api Document URLs

--- scripts/generateDocs.sh
+++ scripts/generateDocs.sh
@@ -3,13 +3,13 @@ set -e
 
 projectGithubUrl="https://github.com/djfdyuruiry/ts-lambda-api-local/blob/master/src"
 tlaGithubUrl="https://github.com/djfdyuruiry/ts-lambda-api/blob/master/src"
-docHtmlPath="./docs/classes/apiconsoleapp.html"
 projectPath="$(pwd)"
+docHtmlPath="${projectPath}/docs/classes/apiconsoleapp.html"
 
 function cleanUpDocUrls() {
-    sed -i "s^${projectGithubUrl}${projectPath}/node_modules/ts-lambda-api/dist/^${tlaGithubUrl}/^" "${docHtmlPath}"
-    sed -i "s^${projectPath}/node_modules/ts-lambda-api/dist/^^" "${docHtmlPath}"
-    sed -i "s^/node_modules/ts-lambda-api/dist/\\([^<]+\\)<^<a href="${tlaGithubUrl}/\\1" /><^" "${docHtmlPath}"
+    sed -i "s^${projectGithubUrl}${projectPath}/node_modules/ts-lambda-api/dist/^${tlaGithubUrl}/^g" "${docHtmlPath}"
+    sed -i "s^${projectPath}/node_modules/ts-lambda-api/dist/^^g" "${docHtmlPath}"
+    sed -i "s%node_modules/ts-lambda-api/dist%${tlaGithubUrl}%g" "${docHtmlPath}"
     sed -E -i "s/.d.ts:[0-9]+/.ts/" "${docHtmlPath}"
     sed -E -i "s/.d.ts#L[0-9]+/.ts/" "${docHtmlPath}"
 }
